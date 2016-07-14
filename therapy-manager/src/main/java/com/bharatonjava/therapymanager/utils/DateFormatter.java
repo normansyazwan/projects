30501cbedcee4f8bbe8c2af92acaa8fc09710d03
Bharat <ibharatsharma@gmail.com>
2016-07-15 01:32:14 +0530

    Added DateFormatter.java to handle null dates in forms

---
+++ therapy-manager/src/main/java/com/bharatonjava/therapymanager/utils/DateFormatter.java
@@ -0,0 +1,17 @@
+package com.bharatonjava.therapymanager.utils;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatter extends org.springframework.format.datetime.DateFormatter {
+
+    @Override
+    public Date parse(String text, Locale locale) throws ParseException {
+        if (text != null && text.isEmpty()) {
+            return null;
+        }
+        return super.parse(text, locale);
+    }
+
+}
